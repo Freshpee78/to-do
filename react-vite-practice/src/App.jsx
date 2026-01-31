6e136b9cdb4b386862983f6074333d26a978a918
freshpee <ogunsolapelumi1@gmail.com>
2026-01-31 10:49:38 +0100

    to-do

---
+++ react-vite-practice/src/App.jsx
@@ -0,0 +1,21 @@
+import Counter from "./components/Counter";
+import TodoList from "./components/TodoList";
+import UserData from "./components/UserData";
+
+function App() {
+  return (
+    <div>
+      <h1>React Vite Practice</h1>
+
+      <Counter />
+      <hr />
+
+      <TodoList />
+      <hr />
+
+      <UserData />
+    </div>
+  );
+}
+
+export default App;
